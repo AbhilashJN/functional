d57ba6b0ed8e155d55af597776c2fcac51f6cd95
AbhilashJN <abhilashjn107@gmail.com>
2018-01-16 10:16:19 +0530

    Test case for proper input passed

---
+++ ex3.js
@@ -0,0 +1,29 @@
+function doubleAll(numbers) {
+
+    var result = []
+   
+    result = numbers.map( function (x){ return 2*x})
+
+    return result
+
+    }
+
+
+
+
+//module.exports = doubleAll
+
+
+function isEqual(first,second)
+{
+	for(let i=0;i<first.length;i++)
+	{
+		if(first[i]!=second[i])
+		{
+			return false
+		}
+	}
+	return true
+}
+
+console.log("should return true for proper input : " , isEqual(doubleAll([1,2,3,4]) , [2,4,6,8])===true   )
