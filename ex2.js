c7dd8191f4faa2bf6caa2c215819a4af52177381
AbhilashJN <abhilashjn107@gmail.com>
2018-01-16 10:01:54 +0530

    Test case passed

---
+++ ex2.js
@@ -0,0 +1,21 @@
+let i=0
+
+function repeat(operation , num)
+{
+	i=num
+	if(num>0)
+	{
+		repeat(operation, num-1)
+		i=i-1
+	}
+	else if(num===0)
+	{
+		return
+	}
+
+}
+
+
+//module.exports = repeat
+
+console.log("Should return true" , i===0)
