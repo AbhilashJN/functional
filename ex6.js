1e8a5bbf6802d47a1938ea6110a704c46f719b32
AbhilashJN <abhilashjn107@gmail.com>
2018-01-17 13:31:40 +0530

    Test case for correc input passed

--- ex6.js
+++ ex6.js
@@ -1,29 +1,31 @@
 function countWords(inputWords) {
-      // SOLUTION GOES HERE
+	// SOLUTION GOES HERE
 
 
-      let result = inputWords.reduce(   function(accumulator , currentValue) {
+	let result = inputWords.reduce(   function(accumulator , currentValue) {
 
-      		if(currentValue in accumulator)
-      		{
-      			accumulator[currentValue] +=1
+		if(currentValue in accumulator)
+		{
+			accumulator[currentValue] +=1;
 
-      		}
-      		else
-      		{
-      			accumulator[currentValue] =1
-      		}
+		}
+		else
+		{
+			accumulator[currentValue] =1;
+		}
+			
+		return accumulator;
+	},
 
-      		
-      		return accumulator
-      },
-
-      {} )
-return result
+	{} );
+	return result;
+	
 
 }
 
-//module.exports = countWords
+//module.exports = countWords;
 
 
 console.log("Should return the correct result object" , countWords(['Apple', 'Banana', 'Apple', 'Durian', 'Durian', 'Durian'])==={Apple : 2,Banana : 1, Durian:3})
+console.log("Should return empty object" );
+console.log(countWords([]));
---
+++ ex8.js
@@ -0,0 +1,30 @@
+function duckCount() {
+	// SOLUTION GOES HERE
+	
+	let args = Array.prototype.slice.call(arguments);
+	
+	if( args.length===0)
+	{
+		return 0;
+	}
+	else{
+		
+		let temp = Object.hasOwnProperty.call(args[0] , "quack")?1:0;
+		
+        
+		temp+= duckCount.apply(null,args.slice(1)); 
+
+		return temp;
+	}
+}
+
+//module.exports = duckCount;
+
+
+
+let notDuck = Object.create({quack: true});
+let duck = {quack: true};
+let duck2 = {quack:true , as:1};
+
+console.log("Should return true for correct input" , duckCount(duck, notDuck)===1);
+
