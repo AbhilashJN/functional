9c9aea6aa5d05ace1ba340422d2576a2991f15ac
AbhilashJN <abhilashjn107@gmail.com>
2018-01-17 13:33:48 +0530

    Test case for 0 matches passed

--- ex8.js
+++ ex8.js
@@ -25,6 +25,7 @@ function duckCount() {
 let notDuck = Object.create({quack: true});
 let duck = {quack: true};
 let duck2 = {quack:true , as:1};
+let duck3= {q:1};
 
 console.log("Should return true for correct input" , duckCount(duck, notDuck)===1);
-
+console.log("Should return true for input with 0 matches ", duckCount(notDuck,duck3)===0);
